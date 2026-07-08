5be47383e28531f9554f4d528fb5f98717ec2189
rohitgoswami07 <rohitgoswami0711@gmail.com>
2026-07-08 19:44:00 +0530

    fix: add kotlin.android plugin and jvmTarget 11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.ksp)
+    alias(libs.plugins.kotlin.android)
 }
 
 android {
@@ -34,6 +35,9 @@ android {
     }
     buildFeatures {
         compose = true
+    }
+    kotlinOptions {
+        jvmTarget = "11"
     }
 }
 
